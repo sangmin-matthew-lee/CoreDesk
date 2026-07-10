17f3a36bbeb43a7acb1fd341dc51e132b741abce
Sangmin Lee <sangmin@Sangmins-MacBook-Air.local>
2026-07-10 13:12:50 -0700

    feat:configure remote GCS backend for persistent tf state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
+  backend "gcs" {} # Configured dynamically in CI/CD pipeline
   required_providers {
     google = {
       source  = "hashicorp/google"
